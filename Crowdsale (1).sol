09c8f8cea588e26b8fdb80680f35af50078a4b38
kap108 <78456331+kap108@users.noreply.github.com>
2021-07-25 15:05:00 -0400

    Add files via upload

---
+++ Crowdsale (1).sol
@@ -0,0 +1,49 @@
+pragma solidity ^0.5.0;
+
+import "./PupperCoin.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/crowdsale/Crowdsale.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/crowdsale/emission/MintedCrowdsale.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/crowdsale/validation/CappedCrowdsale.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/crowdsale/validation/TimedCrowdsale.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/crowdsale/distribution/RefundablePostDeliveryCrowdsale.sol";
+
+// @TODO: Inherit the crowdsale contracts
+contract PupperCoinSale is {
+
+    constructor(
+        uint rate, // rate
+        address payable wallet, // sale beneficiary
+        token // the Token is the TokenSale will work with
+    )
+        Crowdsale(rate, wallet, token)
+        public
+    {
+        // constructor can stay empty
+    }
+}
+
+
+contract PupperCoinSaleDeployer {
+
+    address public token_sale_address;
+    address public token_address;
+
+    constructor(
+        string memory name, 
+        string memory symbol, 
+        address payable wallet) // @TODO: Fill in the constructor parameters!
+    )
+        public
+    {
+        // token = new Token(name, symbol, 0);
+        token_address = address(token);
+
+
+        // arcade_sale = new ArcadeTokenSale(1, wallet, token);
+        arcade_sale_address = address(arcade_sale);
+
+        // make the PupperCoinSale contract a minter, then have the PupperCoinSaleDeployer renounce its minter role
+        token.addMinter(token_sale_address);
+        token.renounceMinter();
+    }
+}
